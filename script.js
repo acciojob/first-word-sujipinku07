9e1e3fe118b32506d577b719b2c7d385bfcab1b9
sujipinku07 <sujipinku07@gmail.com>
2024-09-29 17:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading and trailing spaces to handle strings starting with spaces
+  s = s.trim();
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+  // Return the substring from the start of the string to the first space
+return s.substring(0, spaceIndex);
+ }// your code here
 }
 
 // Do not change the code below
