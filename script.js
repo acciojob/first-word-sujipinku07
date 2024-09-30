68f15a0b3817892fe445e03d5b3f2b1e81130b5e
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 19:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 function firstWord(s) {
-  // Trim leading and trailing spaces to handle strings starting with spaces
-  s = s.trim();
-  // Find the index of the first space
-  const spaceIndex = s.indexOf(' ');
-  // If no space is found, return the entire string
-  if (spaceIndex === -1) {
-    return s;
-  }
-  // Return the substring from the start of the string to the first space
-return s.substring(0, spaceIndex);
- }// your code here
+	s=s.trim();
+    if(s.length == 0){
+        return s
+    }
+    let word = s.split(" ");
+    return word[0];
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+ const s = prompt("Enter String:");
+alert(firstWord(s))
